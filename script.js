532e2c4893199841742318eaf43d631347aec745
Muhammad Fawwaz <fawyna@gmail.com>
2016-05-15 22:19:46 +0800

    Add onload for instafeed code

--- script.js
+++ script.js
@@ -62,7 +62,8 @@ jQuery(window).load(function () {
 
 });
 
-var feed = new Instafeed({
+window.onload(function(){
+  var feed = new Instafeed({
   clientId: '467ede5a6b9b48ae8e03f4e2582aeeb3',
   limit: 20,
   sortBy: 'most-liked',
@@ -81,3 +82,4 @@ var feed = new Instafeed({
   template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /><div class="likes">&hearts; {{likes}}</div></a>'
 });
 feed.run();
+});
